e78e008f6425ff5e40089b648d62b747478b9b21
John Roach <johnroach1985@gmail.com>
2017-11-30 21:53:13 -0500

    Adding some dice stuff

--- dndme.go
+++ dndme.go
@@ -16,6 +16,9 @@ type Command struct {
 	HelpText    string
 }
 
+// commands is a mapping of strings to functions
+var commands = map[string]Command{}
+
 func completer(d prompt.Document) []prompt.Suggest {
 	s := []prompt.Suggest{
 		{Text: "roll", Description: "Rolls dice"},
@@ -30,42 +33,42 @@ func random(min, max int) int {
 
 // roll will roll a dice
 func roll(input []string) {
-	var dice_input []string= strings.Split(input[0], "d")
-	minimum_roll, minimum_roll_err := strconv.Atoi(dice_input[0])
-	maximum_roll, maximum_roll_err := strconv.Atoi(dice_input[1])
-	if minimum_roll_err != nil && maximum_roll_err != nil {
+	var diceInput = strings.Split(input[0], "d")
+	minimumRoll, maximumRollErr := strconv.Atoi(diceInput[0])
+	maximumRoll, maximumRollErr := strconv.Atoi(diceInput[1])
+	if maximumRollErr != nil && maximumRollErr != nil {
 		fmt.Println("You probably didn't enter a valid number for the dice. :(")
 	} else {
-		fmt.Println(random(minimum_roll, maximum_roll * minimum_roll))
+		fmt.Println(random(minimumRoll, maximumRoll * minimumRoll))
 	}
 }
 
 func help(input []string) {
-	fmt.Printf("%v", input)
+	fmt.Println(commands[input[0]].HelpText)
 }
 
 func main() {
-	// commands is a mapping of strings to functions
-	commands := map[string]Command{
+
+	commands = map[string]Command{
 		"roll": {
-			roll,
-			"roll",
-			"Rolls dice",
-			"roll <number of times>d<dice type>"},
+		roll,
+		"roll",
+		"Rolls dice",
+		"roll <number of times>d<dice type>"},
 		"help": {
-			help,
-			"help",
-			"Prints out help text",
-			"help <command>"},
+		help,
+		"help",
+		"Prints out help text",
+		"help <command>"},
 	}
 
 	fmt.Println("Welcome to dndme!")
 
-	prompt_input := strings.Split(prompt.Input("> ", completer), " ")
-	base_command := prompt_input[0]
-	_, ok := commands[base_command]
+	promptInput := strings.Split(prompt.Input("> ", completer), " ")
+	baseCommand := promptInput[0]
+	_, ok := commands[baseCommand]
 	if ok {
-		commands[base_command].Fn(append(prompt_input[:0], prompt_input[1:]...))
+		commands[baseCommand].Fn(append(promptInput[:0], promptInput[1:]...))
 	} else {
 		fmt.Println("key not found")
 	}
